bf17cc29d810e06f397468b28fbdd40fad732ef0
jionjiang <649768017@qq.com>
2021-12-14 18:05:28 +0800

    livewire,tailwindcss, laravel blade 组件配合搭建前端项目

---
+++ app/Http/Livewire/CirclePer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CirclePer extends Component
+{
+    public function render()
+    {
+        return view('livewire.circle-per');
+    }
+}
---
+++ resources/views/circle/circle.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+    <x-slot name="title">
+        circle
+    </x-slot>
+    <livewire:circle-per/>
+</x-layouts.app>
